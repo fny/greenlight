01a4a84076c8aebb4144d3e86748c75d80d90b08
Faraz Yashar <faraz.yashar@gmail.com>
2020-10-13 07:26:44 -0400

    Fix user fabricator phones

--- api/spec/fabricators/user_fabricator.rb
+++ api/spec/fabricators/user_fabricator.rb
@@ -5,6 +5,12 @@ Fabricator(:user) do
   last_name { Faker::Name.last_name }
   time_zone { ActiveSupport::TimeZone.all.map { |x| x.tzinfo.name }.sample }
   email { Faker::Internet.unique.email }
-  mobile_number { Faker::PhoneNumber.unique.cell_phone_in_e164[1..-1] }
+  mobile_number {
+    number = ''
+    while !Phonelib.parse(number, 'US').valid?
+      number = Faker::PhoneNumber.cell_phone_in_e164[3, 10]
+    end
+    Phonelib.parse(number, 'US').full_e164
+  }
   password 'super_secret_password'
 end
