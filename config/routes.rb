193b3753c67bd1799716ef108fe90dcf0083baa1
Faraz Yashar <faraz.yashar@gmail.com>
2020-12-01 13:37:41 -0500

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   mount lambda { |env| APIController.call(env) } => '/'
-  require "sidekiq/web"
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    # Protect against timing attacks:
-    # - See https://codahale.com/a-lesson-in-timing-attacks/
-    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
-    # - Use & (do not use &&) so that it doesn't short circuit.
-    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest('GreenLantern')) &
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV.fetch("SUPERUSER_PASSWORD")))
-  end if Rails.env.production?
-  mount Sidekiq::Web, at: "/sidekiq"
 
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      # Protect against timing attacks:
+      # - See https://codahale.com/a-lesson-in-timing-attacks/
+      # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+      # - Use & (do not use &&) so that it doesn't short circuit.
+      # - Use digests to stop length information leaking(see also
+      #   ActiveSupport::SecurityUtils.variable_size_secure_compare)
+      username1 = ::Digest::SHA256.hexdigest(username)
+      username2 = ::Digest::SHA256.hexdigest('GreenLantern')
+      password1 = ::Digest::SHA256.hexdigest(password)
+      password2 = ::Digest::SHA256.hexdigest(ENV.fetch('SUPERUSER_PASSWORD'))
+      ActiveSupport::SecurityUtils.secure_compare(username1, username2) &
+        ActiveSupport::SecurityUtils.secure_compare(password1, password2)
+    end
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
 end
