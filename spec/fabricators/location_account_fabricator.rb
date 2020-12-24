bd97386629cc9ebc3e7e5381a1df7efc7e677bb7
Faraz Yashar <faraz.yashar@gmail.com>
2020-12-23 22:58:37 -0500

    fix: remove location account fabricator from test

--- spec/fabricators/location_account_fabricator.rb
+++ spec/fabricators/location_account_fabricator.rb
@@ -5,8 +5,6 @@ Fabricator(:location_account) do
   external_id             Faker::Internet.uuid
   role                    "student"
   permission_level        "none"
-  title                   "MyText"
-  attendance_status       "MyText"
   approved_by_user_at     "2020-09-14 17:49:37"
   approved_by_location_at "2020-09-14 17:49:37"
 end
