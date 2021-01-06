d1c45c6abf037cc825d0f12ba326cb3bc4abbe8d
Faraz Yashar <faraz.yashar@gmail.com>
2021-01-06 13:59:30 -0500

    fix: Move loading page content to its own component

--- client/src/pages/util/LoadingPage.tsx
+++ client/src/pages/util/LoadingPage.tsx
@@ -1,28 +1,11 @@
-import { t, Trans } from '@lingui/macro'
-import {
-  Block, Navbar, Page, Preloader,
-} from 'framework7-react'
+import { Page } from 'framework7-react'
 import React from 'react'
-import EmailLink, { SUPPORT_EMAIL } from 'src/components/EmailLink'
-import NavbarHomeLink from 'src/components/NavbarHomeLink'
-import './LoadingPage.css'
+import LoadingPageContent from 'src/components/LoadingPageContent'
 
 export default function LoadingPage({ title }: { title?: string}): JSX.Element {
   return (
     <Page>
-      <Navbar title={title || t({ id: 'LoadingPage.title', message: 'Loading...' })}>
-        <NavbarHomeLink slot="left" />
-      </Navbar>
-      <Block className="LoadingPage container">
-        <div className="LoadingPage item"><Preloader /></div>
-      </Block>
-      <Block>
-        <p>
-          <Trans id="LoadingPage.message">
-            Stuck here for too long? Try refreshing the page. If you still need help please email us at <EmailLink email={SUPPORT_EMAIL} />.
-          </Trans>
-        </p>
-      </Block>
+      <LoadingPageContent title={title} />
     </Page>
   )
 }
