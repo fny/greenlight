e0d90e6bd01c0660a6afa9246fa8e4a087b9de6c
Jeremy <jeremy.he.codes@gmail.com>
2021-01-07 07:30:16 -0500

    patch: report content

--- app/workers/people_report_worker.rb
+++ app/workers/people_report_worker.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-class SchoolReportWorker < ApplicationWorker
-  def perform(user_id, location_id)
-    user = User.find(user_id)
+class PeopleReportWorker < ApplicationWorker
+  def perform(admin_id, location_id)
+    admin = User.find(admin_id)
     location = Location.find(location_id)
 
-    school_report = Reports::School.new(location)
+    people_report = Reports::People.new(location)
     s3_upload = UploadToS3.new(
-      content: school_report.csv,
+      content: people_report.csv,
       path: "reports/#{location.permalink}/#{Time.now.strftime('%Y-%m-%d_%H_%M')}.csv",
       filename: "report(#{location.permalink}).csv",
     )
@@ -17,8 +17,8 @@ class SchoolReportWorker < ApplicationWorker
       download_url = s3_upload.result
 
       SendGridEmail.new(
-        to: user.name_with_email,
-        subject: I18n.t('emails.school_report.subject'),
+        to: admin.name_with_email,
+        subject: I18n.t('emails.people_report.subject'),
         html: eval(html_template),
       ).run
     end
@@ -28,15 +28,15 @@ class SchoolReportWorker < ApplicationWorker
 
   def html_template
     Erubi::Engine.new(<<~HTML).src
-      <h2><%= I18n.t('emails.school_report.title') %></h2>
+      <h2><%= I18n.t('emails.people_report.title') %></h2>
 
       <p>
-        <%= I18n.t('emails.school_report.body', school: location.name) %>
+        <%= I18n.t('emails.people_report.body', location: location.name) %>
       </p>
 
       <p style="font-weight:bold">
         <a href="<%= download_url %>">
-          <%= I18n.t('emails.school_report.action') %>
+          <%= I18n.t('emails.people_report.action') %>
         </a>
       </p>
     HTML
