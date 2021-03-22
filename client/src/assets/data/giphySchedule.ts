fa9d72d28f2294b72bc5ac27a693c72af6b95868
Faraz Yashar <faraz.yashar@gmail.com>
2021-03-22 06:04:06 -0400

    feat: more gifs!

--- client/src/assets/data/giphySchedule.ts
+++ client/src/assets/data/giphySchedule.ts
@@ -1,24 +1,63 @@
 import { Dict } from 'src/types'
 
 const giphySchedule: Dict<string> = {
+  '3/24/2021': 'CQpcrKmeAHDQA',
+  '3/25/2021': '82nxC1u2BC8VU1wiZq',
+  '3/26/2021': '3otPoUQpiPI7iPVIqs',
+  '3/27/2021': 'xHMcjnlst9JqU',
+  '3/28/2021': 'iGdkUBZO1896QiU8pw',
+  '3/29/2021': '8vQSQ3cNXuDGo',
+  '3/30/2021': 'v4joJC0Z76mkM',
+  '3/31/2021': 'nC30bN4yizgek',
+  '4/1/2021': 'D5AJaFQ7N0uZi',
+  '4/2/2021': 'slFnAtVKXoAz6',
+  '4/3/2021': '3Mfad3XHscMRG',
+  '4/4/2021': 'UTlUCyGX93D3i',
+  '4/5/2021': 'Wu7UDoOBHPHm8',
+  '4/6/2021': 'naXyAp2VYMR4k',
+  '4/7/2021': 'fhQDRJCRCgOwU',
+  '4/8/2021': '26FeULfww4ybuIYms',
+  '4/9/2021': '8PA8Ew3nw97yg',
+  '4/10/2021': 'zVN0OolkDHmbC',
+  '4/11/2021': 'vUO3OogY5WwXS',
+  '4/12/2021': 'EatwJZRUIv41G',
+  '4/13/2021': 'l3V0lsGtTMSB5YNgc',
+  '4/14/2021': 'FDHDP7DREKSlYtHm43',
+  '4/15/2021': 'sE6jQonM5S8mI',
+  '4/16/2021': 'S7QPoOt6UOs2rs67PK',
+  '4/17/2021': 'LFiOdYoOlEKac',
+  '4/18/2021': 'stmktFMrXgLVC',
+  '4/19/2021': 'VduFvPwm3gfGO8duNN',
+  '4/20/2021': 'Zcj1D89F3y38EgLKNX',
+  '4/21/2021': 'LfCt1sR1VweBy',
+  '4/22/2021': '26ufb5faQRWH7KOn6',
+  '4/23/2021': 'Yqqd7rRT5n5qE',
+  '4/24/2021': 'PM0w9LCV7h58Q',
+  '4/25/2021': '26UyJlmLGEeGs',
+  '4/26/2021': 'UyioHpfEkhmQo',
+  '4/27/2021': '5cdenDXni65aM',
+  '4/28/2021': 'fM2zG55vH7qwhXdDg2',
+  '4/29/2021': '2blIS3zwdrlHa',
+  '4/30/2021': 'Ran7QWtJ1JCXSgGgn9',
+  '5/1/2021': 'pNniXjcBXfWbbB1YCE',
+  '5/2/2021': 'lqIaUfPi8rTqM',
+  '5/3/2021': 'l3q2u6MXJJEKiTZIY',
+  '5/4/2021': 'H2GT0TQBAlbuo',
+  '5/5/2021': 'xFnAObsU5nMrUUC3HX',
+  '5/6/2021': 'RhrAmVUHxjTQvEPBWi',
+  '5/7/2021': '3oEjHQKtDXpeGN9rW0',
+  '5/8/2021': 'YbXtbKoi2ZUOc',
   '12/2/2020': 'xT0BKsuDrhX7CDYO8U', // Wed
   '12/1/2020': 'xThuWiJqEd2c44AjBu', // Tues
   '11/30/2020': 'xT8qB15vyMbmlOVhsY', // Mon
   '11/29/2020': '3owyp6z5IjCG9dZvtS', // Sun
-  '11/28/2020': '3o6gDREoETcSrm9Wog', // Sat
-  '11/27/2020': 'xFcFJAryrhO6s', // Fri
-  '11/26/2020': 'XGgkpVY62gcwXkOPml', // Thurs
   '11/25/2020': 'QC1TssrPbkD2menNfz', // Wed
-  '11/24/2020': 'v2CaxWLFw4a5y', // Tues
-  '11/23/2020': 'l4JyZjdrJFkVn6x3y', // Mon
-  '11/22/2020': '126NMCvhKJfNN6', // Sun
   '11/21/2020': '26wkQBBppNQ1ih584', // Sat
   '11/20/2020': 'OcldYrF5srXoY', // Fri
   '11/19/2020': 'hQKiGV6MG8WmsHg2yx', // Thurs
   '11/18/2020': 'VzeJ8Zl5kdi9KAD4Xk', // Wed
   '11/17/2020': 'NsEU83HMPEM2ekYwCj', // Tues
   '11/16/2020': 'wqb5K5564JSlW', // Mon
-  '11/15/2020': '1wqqlaQ7IX3TXibXZE', // Sun
   '11/14/2020': 'kiBcwEXegBTACmVOnE', // Sat
   '11/13/2020': 'l0MYNB04rBb51QNtC', // Fri
   '11/12/2020': 'CjmvTCZf2U3p09Cn0h', // Thurs
@@ -31,27 +70,18 @@ const giphySchedule: Dict<string> = {
   '11/5/2020': 'l2GsSPkKjx6ZW', // Thurs
   '11/4/2020': 'OxvYf3jsq8bIc', // Wed
   '11/3/2020': 'hKCgsmlQKNNO8', // Tues
-  '11/2/2020': '1pXVFWycDMoUw', // Mon
-  '11/1/2020': 'SqAM5xE1CNLxe', // Sun
-  '10/31/2020': 'l0MYxI1x4WyCLvUL6',
   '10/30/2020': 'q4ICE9wYvOwBG',
   '10/29/2020': 'LAmpwU0Adqaqc',
-  '10/28/2020': '3oz8xwooUvMqNB1zEs',
   '10/27/2020': '26tP3M3i03hoIYL6M',
   '10/26/2020': '1hqYk0leUMddBBkAM7',
   '10/25/2020': 'rbyCkuoJ6qd7G',
-  '10/24/2020': 'WjldrOwZZ3EpW',
   '10/23/2020': 'l4FGr3nzq5u0m02vm',
   '10/22/2020': '3o7qE2VAxuXWeyvJIY',
   '10/21/2020': 'uBLojRk2fnB84',
-  '10/20/2020': 'j6HJab8c5d8VG',
   '10/19/2020': 'fV5W3pG7F7CwoI3i4B',
   '10/18/2020': 'npFfmAk8yqZbi',
   '10/17/2020': 'cdNSp4L5vCU7aQrYnV',
   '10/16/2020': 'Xw6yFn7frR3Y4',
-  '10/15/2020': 'BZiSRzmpLYKiKo0yyf',
-  '10/14/2020': '10A0XQ5AOSrJao',
-  '10/13/2020': 'i34oXbluCO0G4',
   '10/12/2020': 'tSaApE2vVQKE8',
 }
 
